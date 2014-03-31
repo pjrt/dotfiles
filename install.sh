81b243aa6de184b2ce8a2e9728a8658af1ae482c
Pedro <vondiefinsternis@gmail.com>
2014-03-31 00:54:06 -0400

    New install script
    With a let-if-fail mentality.

--- install.sh
+++ install.sh
@@ -1,25 +1,21 @@
-# Make links
+# Make links from DOTFILES to the correct locations
 
 if [ -z $DOTFILES ]; then
     echo "DOTFILES is not set"
     exit 0
 fi
 
-# Vim stuff
-cd $HOME
-echo "Setting .vimrc"
-ln -sf $DOTFILES/vimrc .vimrc
+export vimrc="vimrc, .vimrc, $HOME"
+export ftplugin="vim_template/ftplugin, ftplugin, $HOME/.vim/"
+export plugin="vim_template/plugin, plugin, $HOME/.vim/"
+export git_template="git_template, .git_template, $HOME"
+export gitconfig="gitconfig, .gitconfig, $HOME"
 
-echo "Setting up .vim"
-cd $HOME/.vim/
-echo "Setting ftplugin and plugin"
-ln -sf $DOTFILES/vim_template/ftplugin ftplugin
-ln -sf $DOTFILES/vim_template/plugin plugin
-
-# Git stuff
-echo "Setting up git"
-cd $HOME
-
-echo "Setting .git_template and .gitconfig"
-ln -sf $DOTFILES/git_template .git_template
-ln -sf $DOTFILES/gitconfig .gitconfig
+IFS=","
+for i in vimrc ftplugin plugin git_template gitconfig;
+do
+    set $i
+    cd $3
+    echo "Setting up $1"
+    ln -s $DOTFILES/$1 $2
+done
