a8806308fcaecdc5d95d8c4860291eaf379d388a
Pedro Rodriguez <pedro@pjrt.co>
2017-04-10 17:32:59 -0400

    Add sbt-ctags plugin and stags modifier

---
+++ sbt13/StagsPlugin.scala
@@ -0,0 +1,32 @@
+import sbt._
+import Keys._
+import com.kalmanb.sbt.CtagsPlugin
+
+object StagsPlugin extends CtagsPlugin {
+
+  /**
+   * [Optional] - Allows you to define where sources are unzipped to.
+   *
+   * @return the directory to store library sources.
+   *         this local to the project base directory
+   */
+  override def ExternalSourcesDir: String = "target/lib-src"
+
+  /**
+   * [Optional] - Allows you to override the function called after lib
+   * sources are added. You can call shell commands such as ctags, gtags,
+   * other indexers or tools. Called after ctagsAdd and ctagsRemove
+   *
+   * @param baseDirectory of the project if you need to access files within
+   *        the project
+   */
+  override def updateCtags(baseDirectory: File): Unit = {
+    /**
+     * Default calls ctags
+     * "ctags" !
+     */
+    "stags -o .git/tags ./".!
+  }
+
+}
+
